4cc12d90b32335397bf473a822c432d4b43ec653
Gabriela Papin <gabrielapapin20@gmail.com>
2022-10-10 16:46:01 -0300

    fix: write return type for each function

--- src/modules/inviteTalent/inviteTalent.controller.ts
+++ src/modules/inviteTalent/inviteTalent.controller.ts
@@ -1,28 +1,29 @@
 import { Body, Controller, Post, Get, Param } from '@nestjs/common';
 import { InviteTalentDto } from 'src/modules/inviteTalent/dto/inviteTalent.dto';
 import { InviteTalentService } from 'src/modules/inviteTalent/inviteTalent.service';
+import { InviteTalentEntity } from 'src/entities/inviteTalent.entity';
 
 @Controller('invite-talent')
 export class InviteTalentController {
   constructor(private inviteTalentService: InviteTalentService) {}
 
   @Get(':id')
-  getInviteMessageById(@Param('id') id: number) {
+  getInviteMessageById(@Param('id') id: number): Promise<InviteTalentEntity> {
     return this.inviteTalentService.getInviteMessageById(Number(id));
   }
 
   @Get('/freelancer/:id')
-  getInviteMessageByProfile(@Param('id') id: number) {
+  getInviteMessageByProfile(@Param('id') id: number): Promise<InviteTalentEntity[]> {
     return this.inviteTalentService.getInviteMessageByProfile(Number(id));
   }
 
   @Get()
-  getAllInviteMessages() {
+  getAllInviteMessages(): Promise<InviteTalentEntity[]> {
     return this.inviteTalentService.getAllInviteMessages();
   }
 
   @Post()
-  saveInviteMessage(@Body() inviteTalentDto: InviteTalentDto) {
+  saveInviteMessage(@Body() inviteTalentDto: InviteTalentDto): Promise<InviteTalentEntity> {
     return this.inviteTalentService.saveInviteMessage(inviteTalentDto);
   }
 }
