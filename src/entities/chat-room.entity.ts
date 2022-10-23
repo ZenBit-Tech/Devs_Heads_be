9bcfbb048ed4fe21bc0e772b7e128a06363cd558
Vadim Vladimirov <kurtonchik@gmail.com>
2022-10-23 22:10:19 +0300

    added docs for new controllers

--- src/entities/chat-room.entity.ts
+++ src/entities/chat-room.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, PrimaryGeneratedColumn, CreateDateColumn, OneToMany, JoinColumn, ManyToOne, Column } from 'typeorm';
 import { JobPostEntity } from './jobPost.entity';
 import { Message } from './message.entity';
@@ -5,27 +6,34 @@ import { User } from './user.entity';
 
 @Entity()
 export class ChatRoom {
+  @ApiProperty({ example: 1, description: 'Uniq identificator' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ type: JobPostEntity })
   @ManyToOne(() => JobPostEntity)
   @JoinColumn({ name: 'jobPostId' })
   jobPostId: JobPostEntity;
 
+  @ApiProperty({ type: User })
   @ManyToOne(() => User)
   @JoinColumn({ name: 'senderId' })
   senderId: User;
 
+  @ApiProperty({ type: User })
   @ManyToOne(() => User)
   @JoinColumn({ name: 'receiverId' })
   receiverId: User;
 
+  @ApiProperty({ type: [Message] })
   @OneToMany(() => Message, (message) => message.chatRoom)
   message: Message[];
 
+  @ApiProperty({ example: false, description: 'Status of active room or not' })
   @Column({ default: false })
   activeRoom: boolean;
 
+  @ApiProperty({ example: '2022-01-01-T12:00:00.000Z', description: 'When created' })
   @CreateDateColumn()
   createdAt: Date;
 }
--- src/modules/chat-room/chat-room.controller.ts
+++ src/modules/chat-room/chat-room.controller.ts
@@ -1,31 +1,43 @@
 import { Body, Controller, Get, Param, Post, Patch } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ChatRoom } from 'src/entities/chat-room.entity';
 import { ChatRoomService } from './chat-room.service';
 import { ChatRoomDto } from './dto/chat-room.dto';
 
+@ApiTags('Chat')
 @Controller('chat-room')
 export class ChatRoomController {
   constructor(private chatRoomService: ChatRoomService) {}
 
+  @ApiOperation({ summary: 'Create chat room' })
+  @ApiResponse({ status: 200, type: ChatRoom })
   @Post()
   createRoom(@Body() data: ChatRoomDto): Promise<ChatRoom> {
     return this.chatRoomService.createRoom(data);
   }
 
+  @ApiOperation({ summary: 'Update chat room bu Id' })
   @Patch(':id')
   updateRoom(@Param('id') id: number, @Body() data: Partial<ChatRoomDto>): Promise<ChatRoom> {
     return this.chatRoomService.updateRoom(Number(id), data);
   }
 
+  @ApiOperation({ summary: 'Get all chat rooms' })
+  @ApiResponse({ status: 200, type: [ChatRoom] })
   @Get()
   getAll() {
     return this.chatRoomService.getAll();
   }
 
+  @ApiOperation({ summary: 'Get chat room by Id' })
+  @ApiResponse({ status: 200, type: [ChatRoom] })
   @Get(':id')
   getChatRoomsByUserId(@Param('id') id: number) {
     return this.chatRoomService.getChatRoomsByUserId(Number(id));
   }
+
+  @ApiOperation({ summary: 'Get chat room by jobPost Id' })
+  @ApiResponse({ status: 200, type: ChatRoom })
   @Get(':senderId/:receiverId/:jobPostId')
   getChatRoomsByTwoUserId(
     @Param('senderId') senderId: number,
