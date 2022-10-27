1adbe2f2c6f787083fac713c3128553a1e97f7a6
Yulia Shyshka <shyshka20@gmail.com>
2022-10-27 15:37:26 +0300

    add changes

--- src/modules/offer/dto/offer.types.ts
+++ src/modules/offer/dto/offer.types.ts
@@ -1,6 +1,8 @@
 export enum Status {
   PENDING = 'Pending',
   ACCEPTED = 'Accepted',
+  ACCEPT = 'In Process',
+  ENDED = 'Ended',
   REJECTED = 'Rejected',
   EXPIRED = 'Expired',
   DELETED = 'Deleted',
